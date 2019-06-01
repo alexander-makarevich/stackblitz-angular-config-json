be0e160603df562655b4ec521470a4a35f422c3d
alexander-makarevich <alexander.s.makarevich@gmail.com>
2019-06-01 13:40:18 +0700

    apply url parameter

--- src/app/settings.service.ts
+++ src/app/settings.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
+interface Config {
+  url: string;
+}
+
 @Injectable()
 export class SettingsService {
+  url: string;
+
   constructor(private http: HttpClient) { }
 
   load(): Promise<any> {
-    return this.http.get('/assets/config.json')
+    return this.http.get<Config>('/assets/config.json')
       .toPromise()
       .then(res => {
-        console.log('Response from server API: ', res);
+        this.url = res.url;
+
         return res;
       });
   }
